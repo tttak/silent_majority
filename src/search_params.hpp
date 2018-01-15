e6b994b366114ac0139bb78b86e59cf27c97e744
k_ohga <ohga@so-la.net>
2018-01-15 15:05:16 +0900

    Tweak: コメントを修正

--- src/search_params.hpp
+++ src/search_params.hpp
@@ -1,15 +1,17 @@
 #ifndef SM_SEARCH_PARAMS_HPP
 #define SM_SEARCH_PARAMS_HPP
 
-// いまの asc は ogaohga まで固定、 PARAM_CUTNODE_REDUCTION_MARGIN  を生かし。
-// いまの desc は 上記の下限を Yane 方向に寄せて、上を伸ばす
-
+// --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< ---
+// step7
+// Futility pruning: child node (skipped when in check)
 //#define PARAM_FUTILITY_RETURN_DEPTH 10 // YaneuraOu
 //#define PARAM_FUTILITY_RETURN_DEPTH 7
 //#define PARAM_FUTILITY_RETURN_DEPTH (int)(progress > 0.64 ? 7 : (progress > 0.16 ? 10 : 13))
 //#define PARAM_FUTILITY_RETURN_DEPTH (int)(progress > 0.64 ? 13 : (progress > 0.16 ? 10 : 7))
 #define PARAM_FUTILITY_RETURN_DEPTH (int)(progress > 0.64 ? 15 : (progress > 0.16 ? 10 : 9))
 
+
+// --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< ---
 // step8
 // null move
 //#define PARAM_NULL_MOVE_MARGIN 32 // YaneuraOu
@@ -29,6 +31,7 @@
 #define PARAM_NULL_MOVE_RETURN_DEPTH (int)(progress > 0.64 ? 20 : (progress > 0.16 ? 12 : 8))
 
 
+// --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< ---
 // step9
 // probcut
 #define PARAM_PROBCUT_DEPTH 7 // YaneuraOu
@@ -42,11 +45,14 @@
 //#define PARAM_PROBCUT_MARGIN (int)(progress > 0.64 ? 220 : (progress > 0.16 ? 210 : 200))
 #define PARAM_PROBCUT_MARGIN (int)(progress > 0.64 ? 240 : (progress > 0.16 ? 220 : 210))
 
+
+// --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< ---
 // step10
 // internal iterative deepening
 #define PARAM_IID_MARGIN_ALPHA 256 // 256 -> 251 -> 256
 
 
+// --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< ---
 // moves_loop
 
 #define PARAM_SINGULAR_EXTENSION_DEPTH 8 // YaneuraOu
@@ -54,11 +60,13 @@
 //#define PARAM_SINGULAR_EXTENSION_DEPTH (int)(progress > 0.64 ? 9 : (progress > 0.16 ? 8 : 7))
 
 
+// --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< ---
 // step11
 // Loop through moves
 #define PARAM_PRUNING_BY_MOVE_COUNT_DEPTH 16 // 16 -> 16
 
 
+// --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< ---
 // step13
 // futility pruning
 
@@ -78,6 +86,8 @@
 #define PARAM_FUTILITY_AT_PARENT_NODE_GAMMA1 40 // 35 -> 40
 #define PARAM_FUTILITY_AT_PARENT_NODE_GAMMA2 51
 
+
+// --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< ---
 // step15
 // LMR
 //#define PARAM_CUTNODE_REDUCTION_MARGIN 2 // YaneuraOu
@@ -91,6 +101,7 @@
 //#define PARAM_REDUCTION_BY_HISTORY (int)(progress > 0.64 ? 6000 : (progress > 0.16 ? 5000 : 4000))
 
 
+// --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< --- 8< ---
 // qsearch
 #define PARAM_FUTILITY_MARGIN_QUIET 145 // 128 -> 145
 
